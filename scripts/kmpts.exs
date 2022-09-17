8ae3fa03829e6dcefde93deddb8f473625aef3bd
Samuel Ventura <samuel.ventura@yeico.com>
2022-09-17 01:13:08 -0500

    Enabled term mouse on keymon.

--- scripts/kmpts.exs
+++ scripts/kmpts.exs
@@ -4,7 +4,7 @@
 alias Teletype.Pts
 
 pts = Pts.open()
-
+Pts.write!(pts, "\ec\e[?25l\e[?1000h\e[?1006h\e[3 q")
 Enum.any?(Stream.cycle(0..1), fn _ ->
   data = Pts.read!(pts)
   IO.puts("#{inspect(data)}\r")
