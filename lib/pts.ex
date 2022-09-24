9ef593302a68b0dff6bac5f7d3c859aec7e32507
Samuel Ventura <samuel.ventura@yeico.com>
2022-09-24 14:14:33 -0500

    Nif disable flag.

--- lib/pts.ex
+++ lib/pts.ex
@@ -1,14 +1,27 @@
 defmodule Teletype.Pts do
   alias Teletype.Nif
 
+  # nif = false disables nif calls
   def open(opts \\ []) do
-    nif = Nif.ttypath()
-    tty = Keyword.get(opts, :tty, nif)
+    nif = Keyword.get(opts, :nif, true)
+
+    {tty, nif} =
+      case nif do
+        false ->
+          tty = Keyword.fetch!(opts, :tty)
+          {tty, false}
+
+        true ->
+          nif = Nif.ttypath()
+          tty = Keyword.get(opts, :tty, nif)
+          {tty, tty == nif}
+      end
+
     exec = :code.priv_dir(:teletype) ++ '/pts'
     opts = [:binary, :exit_status, :stream, args: [tty]]
     port = Port.open({:spawn_executable, exec}, opts)
-    if tty == nif, do: Nif.ttysignal()
-    {port, tty == nif}
+    if nif, do: Nif.ttysignal()
+    {port, nif}
   end
 
   def read!({port, _}) do
@@ -31,7 +44,7 @@ defmodule Teletype.Pts do
   def handle({port, _} = pts, {port, {:data, data}}), do: {pts, :data, data}
   def handle(pts, _), do: {pts, false}
 
-  def close({port, reset}) do
+  def close({port, nif}) do
     # exception if port native process has died
     # ** (ArgumentError) argument error :erlang.port_close(#Port<0.6>)
     # let the user choose to assert :ok = to except on close error
@@ -41,7 +54,7 @@ defmodule Teletype.Pts do
     rescue
       e -> {:error, e}
     after
-      if reset, do: Nif.ttyreset()
+      if nif, do: Nif.ttyreset()
     end
   end
 end
